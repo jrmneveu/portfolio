e6bf0c5db9c0c2f8febe298b13c22bbc28e7e954
jrmneveu <jdp.neveu@gmail.com>
2017-03-19 15:44:33 +0100

    Add BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-// SASS
+// Sass
 gulp.task('sass', function () {
   return gulp.src('./src/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./src/css'));
+    .pipe(gulp.dest('./src/css'))
+    .pipe(browserSync.stream());
 });
+
+// Browser Sync : Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Default Task
+gulp.task('default', ['serve']);
