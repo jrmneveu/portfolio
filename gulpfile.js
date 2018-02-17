00895b069e530e6396b17cf9864e1347fd30de84
Jeremy <jdp.neveu@gmail.com>
2018-02-17 17:03:08 +0100

    Clean all files and folders

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,18 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
-// Sass
+gulp.task('serve', ['sass'], function () {
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("./src/scss/*.scss", ['sass']);
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
 gulp.task('sass', function () {
-  return gulp.src('./src/scss/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./src/css'))
+  return gulp.src("./src/scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./src/css"))
     .pipe(browserSync.stream());
 });
-
-// Browser Sync : Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-
-    gulp.watch("src/scss/**/*.scss", ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
-});
-
-// Default Task
-gulp.task('default', ['serve']);
